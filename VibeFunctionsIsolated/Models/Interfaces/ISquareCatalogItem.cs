6f45695239440f153e44abd16a36f287f53a4a24
Neville Bamshoot <youcanteat@hotmail.com>
2025-02-12 22:36:10 -0500

    re added method to square util

--- VibeFunctionsIsolated/Models/Interfaces/ISquareCatalogItem.cs
+++ VibeFunctionsIsolated/Models/Interfaces/ISquareCatalogItem.cs
@@ -5,6 +5,6 @@ public interface ISquareCatalogItem
     public string Id { get; set; }
     public string Name { get; set; } 
     public string? Description { get; set; }
-    string ImageURL { get; set; }
+    string? ImageURL { get; set; }
 }
 
