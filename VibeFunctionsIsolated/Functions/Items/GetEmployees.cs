718e16fbf82dc148600e706ea16a7257e672982e
Neville Bamshoot <youcanteat@hotmail.com>
2025-02-16 13:47:55 -0500

    Added summary to get employees

--- VibeFunctionsIsolated/Functions/Items/GetEmployees.cs
+++ VibeFunctionsIsolated/Functions/Items/GetEmployees.cs
@@ -3,13 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Square.Models;
 using System.Text.Json;
-using VibeFunctionsIsolated.Models;
-using VibeFunctionsIsolated.Utility;
-using VibeFunctionsIsolated.Enums;
 using VibeFunctionsIsolated.DAL.Interfaces;
+using VibeFunctionsIsolated.Enums;
+using VibeFunctionsIsolated.Models;
 
 namespace VibeFunctionsIsolated.Functions.Items;
 
+/// <summary>
+/// Get all employees from the Square API
+/// </summary>
 public class GetEmployees
 {
     private readonly ISquareSdkDataAccess squareDAL;
@@ -19,74 +21,74 @@ public class GetEmployees
         this.squareDAL = squareDAL;
     }
 
-[Function("GetEmployees")]
-public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
-{
-    SearchCatalogItemsRequest requestBody = buildRequestBody();
-    SearchCatalogItemsResponse? response = await squareDAL.SearchCatalogItems(requestBody);
-
-    IEnumerable<SquareEmployee>? employees = modelEmployees(response);
-
-    if (employees == null) 
+    [Function("GetEmployees")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
     {
-        return new NotFoundResult();
-    }
+        SearchCatalogItemsRequest requestBody = buildRequestBody();
+        SearchCatalogItemsResponse? response = await squareDAL.SearchCatalogItems(requestBody);
 
-    string json = JsonSerializer.Serialize(employees);
+        IEnumerable<SquareEmployee>? employees = modelEmployees(response);
 
-    return new OkObjectResult(json);
-}
-
-// Pair down response data to limit data exposure
-// response - response from the square API
-private IEnumerable<SquareEmployee>? modelEmployees(SearchCatalogItemsResponse? response)
-{
-    if(response?.Items == null || response.Items.Count <= 0)
-    {
-        return null;
-    }
-
-        IEnumerable<SquareEmployee> squareEmployees = response.Items.Select(responseItem =>
+        if (employees == null) 
         {
-            // The custom attribute dictionary uses unpredictable keys so I'm using linq on the list instead to get values during construction
-            IEnumerable<CatalogCustomAttributeValue> customAttributeValues = responseItem.ItemData.Variations[0].CustomAttributeValues.Values;
-            string? imageId;
-            if(responseItem.ItemData?.ImageIds != null)
+            return new NotFoundResult();
+        }
+
+        string json = JsonSerializer.Serialize(employees);
+
+        return new OkObjectResult(json);
+    }
+
+    // Pair down response data to limit data exposure
+    // response - response from the square API
+    private IEnumerable<SquareEmployee>? modelEmployees(SearchCatalogItemsResponse? response)
+    {
+        if(response?.Items == null || response.Items.Count <= 0)
+        {
+            return null;
+        }
+
+            IEnumerable<SquareEmployee> squareEmployees = response.Items.Select(responseItem =>
             {
-                imageId = responseItem.ItemData?.ImageIds[0];
-            }
-            else
-            {
-                imageId = "";
-            }
-            string? imageURL = squareDAL.GetImageURL(imageId).Result;
+                // The custom attribute dictionary uses unpredictable keys so I'm using linq on the list instead to get values during construction
+                IEnumerable<CatalogCustomAttributeValue> customAttributeValues = responseItem.ItemData.Variations[0].CustomAttributeValues.Values;
+                string? imageId;
+                if(responseItem.ItemData?.ImageIds != null)
+                {
+                    imageId = responseItem.ItemData?.ImageIds[0];
+                }
+                else
+                {
+                    imageId = "";
+                }
+                string? imageURL = squareDAL.GetImageURL(imageId).Result;
                 
-            return new SquareEmployee(responseItem, customAttributeValues, imageURL);
-        })
-        .ToList();
+                return new SquareEmployee(responseItem, customAttributeValues, imageURL);
+            })
+            .ToList();
 
-    return squareEmployees;
-}
+        return squareEmployees;
+    }
 
-// Add ids and product type to narrow down results
-private SearchCatalogItemsRequest buildRequestBody()
-{
-    var categoryIds = new List<string>()
+    // Add ids and product type to narrow down results
+    private SearchCatalogItemsRequest buildRequestBody()
     {
-        // Id for Employee category
-        "BJMQNUV2IRXQ4LQLY3BD72ED"
-    };
+        var categoryIds = new List<string>()
+        {
+            // Id for Employee category
+            "BJMQNUV2IRXQ4LQLY3BD72ED"
+        };
 
-    var productTypes = new List<string>()
-    {
-        SquareProductType.AppointmentsService
-    };
+        var productTypes = new List<string>()
+        {
+            SquareProductType.AppointmentsService
+        };
 
-    SearchCatalogItemsRequest body = new SearchCatalogItemsRequest.Builder()
-      .CategoryIds(categoryIds)
-      .ProductTypes(productTypes)
-      .Build();
+        SearchCatalogItemsRequest body = new SearchCatalogItemsRequest.Builder()
+          .CategoryIds(categoryIds)
+          .ProductTypes(productTypes)
+          .Build();
 
-    return body;
-}
+        return body;
+    }
 }
