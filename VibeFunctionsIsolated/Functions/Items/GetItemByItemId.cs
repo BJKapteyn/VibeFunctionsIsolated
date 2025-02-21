d3d65e83be5ed1f72d2ec5b4318a2ddaa3b943e1
Neville Bamshoot <youcanteat@hotmail.com>
2025-02-21 14:14:29 -0500

    cleanup

--- VibeFunctionsIsolated/Functions/Items/GetItemByItemId.cs
+++ VibeFunctionsIsolated/Functions/Items/GetItemByItemId.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Metadata;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
