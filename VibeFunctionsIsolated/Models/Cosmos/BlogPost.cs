e94153da0a5e844e3841430e903e27729dd9aada
Neville Bamshoot <youcanteat@hotmail.com>
2025-08-03 15:45:30 -0400

    edit blogpost serialization

--- VibeFunctionsIsolated/Models/Cosmos/BlogPost.cs
+++ VibeFunctionsIsolated/Models/Cosmos/BlogPost.cs
@@ -22,11 +22,16 @@ public class BlogPost : ICosmosItem
         Content = content;
         ImageUrl = imageUrl;
     }
-
+    [JsonPropertyName("id")]
     public string id { get; set; }
+    [JsonPropertyName("Title")]
     public string Title { get; set; }
+    [JsonPropertyName("Author")]
     public string Author { get; set; }
+    [JsonPropertyName("PublishDate")]
     public DateTime PublishDate { get; set; }
+    [JsonPropertyName("Content")]
     public string Content { get; set; }
+    [JsonPropertyName("ImageUrl")]
     public string? ImageUrl { get; set; }
 }
